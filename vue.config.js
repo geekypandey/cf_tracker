40c98df4a10e92ec7668f6f4502baeba8d6446d8
Anurag Pandey <anuragcarbon1998@gmail.com>
2021-04-27 09:05:40 +0530

    Single File Template Setup for Vue.js using vue-cli

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    pwa: {
+        name: 'MyApp',
+        iconPaths: {
+            favicon32: null,
+            favicon16: null,
+            appleTouchIcon: null,
+            maskIcon: null,
+            msTileImage: null
+        }
+    }
+};
