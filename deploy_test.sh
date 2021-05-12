d558355dea927367c748da0455166d9cbe9e8d59
Anurag Pandey <anuragcarbon1998@gmail.com>
2021-05-12 15:22:26 +0530

    updated deploy.sh and added deploy_test.sh for testing purposes on deploy.git

--- deploy_test.sh
+++ deploy_test.sh
@@ -5,12 +5,11 @@ set -e
 npm run build
 
 cd dist
-echo "cf-tracker.tech" > CNAME
 
 git init .
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:geekypandey/cf_tracker.git master:gh-pages
+git push -f git@github.com:geekypandey/deploy.git master:gh-pages
 
 cd -
