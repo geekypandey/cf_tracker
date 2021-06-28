217931a374c6b0ecbec7e22670b67db2491a3f5d
Anurag Pandey <anuragcarbon1998@gmail.com>
2021-06-28 21:03:11 +0530

    updated get_contests.py script and added workflow for fetching blacklisted contests

--- scripts/get_contests.py
+++ scripts/get_contests.py
@@ -1,16 +1,18 @@
 import os
 import json
 import time
+import itertools
 
 import requests
-from bs4 import BeautifulSoup as bs
+import pandas as pd
+import numpy as np
 
 
 class BlacklistError(Exception):
     pass
 
-
 def get_all_contests():
+    """Fetch all the contests using codeforces api"""
     url = "https://codeforces.com/api/contest.list"
     res = requests.get(url)
     if res.status_code != 200:
@@ -18,39 +20,7 @@ def get_all_contests():
     res = res.json()
     if res["status"] != "OK":
         raise RuntimeError(f"Response from codeforces, status: {res['status']}")
-    contests = res["result"]
-    return contests
-
-
-def filter_contests(contests, completed=True):
-    if completed:
-        contests = [contest for contest in contests if contest["phase"] != "BEFORE"]
-    else:
-        contests = [contest for contest in contests if contest["phase"] == "BEFORE"]
-    return contests
-
-
-def get_unprocessed_contests(contests, processed_contests):
-    if not processed_contests:
-        return contests
-    processed_ids = {contest["contestId"] for contest in processed_contests}
-    contests = [contest for contest in contests if contest["id"] not in processed_ids]
-    return contests
-
-
-def remove_blacklisted(contests, blacklisted):
-    if not blacklisted:
-        return contests
-    contests = [contest for contest in contests if contest["id"] not in blacklisted]
-    return contests
-
-
-def get_processed_and_blacklisted_contests(filename):
-    if not os.path.exists(filename):
-        return []
-    with open(filename) as f:
-        result = json.load(f)
-    return (result.get("contests", []), result.get("blacklisted", []))
+    return res["result"]
 
 
 def get_required_fields(contest, fields, as_fields):
@@ -70,25 +40,35 @@ def get_contest_division(contest):
     return div
 
 
+def replace_nan(x):
+    if isinstance(x, str):
+        return x
+    if np.isnan(x):
+        return None
+    return x
+
+
+def strip_x(x: str):
+    if not isinstance(x, str):
+        return x
+    return x.strip('x').strip()
+
+
 def get_problems(contest):
-    idx = contest["id"]
-    url = "https://codeforces.com/contest/" + str(idx)
-    res = requests.get(url)
-    if res.status_code != 200:
-        raise RuntimeError(f"Request status code: {res.status_code}")
-    soup = bs(res.text, "html.parser")
-    problems = []
-    try:
-        trs = soup.find("table", {"class": "problems"}).find_all("tr")[1:]
-        for tr in trs:
-            index = tr.find("td").text.strip()
-            solvedCount = tr.find(
-                "a", {"title": "Participants solved the problem"}
-            ).text.strip()[1:]
-            p = {"index": index, "solvedCount": solvedCount}
-            problems.append(p)
-    except Exception:
-        raise BlacklistError(f"{idx} is blacklisted")
+    idx = contest['id']
+    url = f'https://codeforces.com/contest/{idx}'
+    df = pd.read_html(url)[1]
+    if 'Name' not in df.columns:
+        raise BlacklistError(f'{idx} is blacklisted')
+    df.dropna(axis=1, how='all', inplace=True)
+    columns = ['idx', 'name', 'solved_count']
+    df.columns = [columns[i] for i in range(len(df.columns))]
+    indices = [str(val) for val in df['idx'].values]
+    solved_count = []
+    if 'solved_count' in df.columns:
+        df['solved_count'] = df['solved_count'].apply(strip_x)
+        solved_count = df['solved_count'].apply(replace_nan).values
+    problems = [{"index": idx, "solvedCount": count} for idx, count in itertools.zip_longest(indices, solved_count)]
     return problems
 
 
@@ -98,42 +78,62 @@ def process_contests(contests):
     fields = ["id", "name", "startTimeSeconds"]
     as_fields = ["contestId", "name", "startTimeSeconds"]
     for idx, contest in enumerate(contests, start=1):
-        print(idx, contest["id"])
         time.sleep(0.2)
         c = get_required_fields(contest, fields=fields, as_fields=as_fields)
         c["div"] = get_contest_division(contest)
         try:
             c["problems"] = get_problems(contest)
         except BlacklistError:
+            print(f"{idx} : {contest['id']} blacklisted!")
             blacklisted.append(contest["id"])
             continue
         contest_data.append(c)
+        print(f"{idx} : {contest['id']} completed!")
     return (contest_data, blacklisted)
 
 
 if __name__ == "__main__":
-    filename = "contests.json"
+    FILENAME = "contests.json"
+
+    # get all contests
     contests = get_all_contests()
-    contests = filter_contests(contests, completed=True)
 
-    processed_contests, blacklisted = get_processed_and_blacklisted_contests(filename)
+    # select only completed ones
+    contests = [contest for contest in contests if contest['phase'] != 'BEFORE']
 
-    contests = get_unprocessed_contests(contests, processed_contests)
-    contests = remove_blacklisted(contests, blacklisted)
+    # remove processed contests
+    completed_ids = []
+    blacklisted = []
+    if os.path.exists(FILENAME):
+        with open(FILENAME) as f:
+            result = json.load(f)
+        processed_contests = result.get('contests', [])
+        completed_ids = [contest['contestId'] for contest in processed_contests]
+        blacklisted_ids = result.get('blacklisted', [])
+
+    contests = [contest for contest in contests if contest['id'] not in completed_ids]
+
+    # if not try blacklisted, remove the blacklisted ids
+    if not os.environ.get('TRY_BLACKLISTED'):
+        contests = [contest for contest in contests if contest['id'] not in blacklisted_ids]
 
     if not contests:
         print('No new contests for processing. Done.')
         exit(1)
 
     print(f'Processing {len(contests)} new contests')
-    new_contests, new_blacklisted = process_contests(contests)
+    new_contests, new_blacklisted_ids = process_contests(contests)
 
-    print(f'Processed! {len(new_contests)} added and {len(new_blacklisted)} blacklisted.')
+    print(f'Processed! {len(new_contests)} added and {len(new_blacklisted_ids)} blacklisted.')
 
     contests = processed_contests + new_contests
-    blacklisted = blacklisted + new_blacklisted
+    blacklisted_ids = blacklisted_ids + new_blacklisted_ids
 
-    json_data = {"contests": contests, "blacklisted": blacklisted}
+    contests_ids = {contest['contestId'] for contest in contests}
 
-    with open(filename, "w") as f:
+    blacklisted_ids = [idx for idx in blacklisted_ids if idx not in contests_ids]
+
+    json_data = {"contests": contests, "blacklisted": blacklisted_ids}
+
+    with open(FILENAME, "w") as f:
         json.dump(json_data, f)
