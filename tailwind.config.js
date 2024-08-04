bf7ac27835670b53c7aefdea489bb4285e846374
Anurag Pandey <anuragcarbon1998@gmail.com>
2024-08-05 00:27:30 +0530

    update colors for using in project

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+            'blue-header': '#001272',
+        },
+    },
   },
   plugins: [],
 }
