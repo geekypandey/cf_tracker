bb119a93b02225e54c0e02b83a1890a24af5a05f
Anurag Pandey <anuragcarbon1998@gmail.com>
2024-08-17 17:35:12 +0530

    add service to manage api calls with codeforces

---
+++ src/services/CodeforcesApiService.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+const BASE_URL = 'https://codeforces.com/api'
+
+
+const getCodeforcesClient = (endpoint, params) => {
+    return axios({
+        baseURL: BASE_URL,
+        method: 'get',
+        url: endpoint,
+        params: params,
+    })
+}
+
+export const getUserInfo = async (usernames) => {
+    const response = await getCodeforcesClient('/user.info', {
+        handles: usernames.join(';')
+    })
+    if (response.status === 200) {
+        return response.data.result;
+    }
+    return [];
+}
