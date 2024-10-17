a11333cf6924f8af9d0e0cc999fdc07a1f36d2b4
Anurag Pandey <anuragcarbon1998@gmail.com>
2024-10-17 14:08:21 +0530

    remove old code and use composition API

--- src/components/DisplayContests.vue
+++ src/components/DisplayContests.vue
@@ -1,166 +1,31 @@
-<script>
-import DisplayFilters from "@/components/DisplayFilters.vue";
+<script setup>
 import ContestsData from "@/data/contests.json";
-import Problem from '@/models/Problem'
-import Contest from '@/models/Contest'
 
-export default {
-  components: {
-    DisplayFilters,
-  },
-
-  props: {
-    usernames: {
-      type: Array,
-      required: true,
-      default: function () {
-        return [];
-      },
-    },
-  },
-
-  data() {
-    return {
-      contests: [],
-      selected: [],
-      picked: "all",
-    };
-  },
-
-  methods: {
-    display(contest) {
-      return (
-        (!this.selected.length || this.selected.includes(contest.div)) &&
-        (this.picked == "all"
-          ? true
-          : this.picked == "active"
-          ? contest.active
-          : contest.passive)
-      );
-    },
-
-    resetContests() {
-      this.contests.forEach(function (contest) {
-        contest.active = false;
-        contest.passive = false;
-        contest.problems.forEach((problem) => (problem.solved = 0));
-      });
-    },
-
-    populateSub(submissions) {
-      // create a dictionary for submissions
-      let map = new Map();
-      for (let sub of submissions) {
-        let c = 0;
-        let key = sub.problem.contestId + sub.problem.index;
-        // clever but not understandable
-        if (sub.verdict == "OK") {
-          c++;
-        } else {
-          c--;
-        }
-
-        if (sub.author.participantType == "CONTESTANT") {
-          if (sub.verdict == "OK") c++;
-          else c--;
-        }
-        if (map.has(key)) {
-          map.set(key, Math.max(map.get(key), c));
-        } else {
-          map.set(key, c);
-        }
-      }
-
-      for (let contest of this.contests) {
-        for (let problem of contest.problems) {
-          let key = contest.id + problem.index;
-          if (!map.has(key)) continue;
-          let val = map.get(key);
-          problem.solved = val;
-          if (val > 0) contest.passive = true;
-          if (val == -2 || val == 2) contest.active = true;
-        }
-      }
-    },
-
-    async refresh() {
-      for (let username of this.usernames) {
-        let submissions = await this.getUserSubmissions(username);
-        this.populateSub(submissions);
-      }
-    },
-
-    async getUserSubmissions(username) {
-      const user_status_url = "https://codeforces.com/api/user.status?handle=";
-      username = username.trim();
-      const submissions = await fetch(user_status_url + username)
-        .then((res) => {
-          if (res.ok) {
-            return res.json();
-          } else {
-            throw new Error(`Username  ${username} not found!`);
-          }
-        })
-        .then((data) => {
-          return data["result"];
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-      return submissions;
-    },
-  },
-
-  computed: {
-    filteredContests() {
-      return this.contests.filter((contest) => this.display(contest));
-    },
-  },
-
-  watch: {
-    async usernames() {
-      this.resetContests();
-      for (let username of this.usernames) {
-        let submissions = await this.getUserSubmissions(username);
-        this.populateSub(submissions);
-      }
-    },
-  },
-
-  created() {
-    // load the data at time of creation
-    let data = ContestsData.contests;
-    data.sort((a, b) => b.startTimeSeconds - a.startTimeSeconds); // change this sort function
-    let contests = [];
-    for (let d of data) {
-      if (d.phase === 'BEFORE') continue;
-      var c = new Contest(d.id, d.name, d.div);
-      if (d.problems) {
-          d.problems.sort((a, b) => a.index.localeCompare(b.index))
-          for (let problem of d.problems) {
-            c.addProblem(problem.index, problem.solvedCount, problem.name);
-          }
-      }
-      contests.push(c);
-    }
-    this.contests = contests;
-  },
-};
+const contests = ContestsData.contests;
 </script>
 
 <template>
   <div v-if="!contests.length" style="text-align: center">Loading.....</div>
   <table class="w-full">
-      <tr v-for="(contest, idx) in filteredContests" class="border border-black h-12">
+      <tr v-for="(contest, idx) in contests" class="border border-black h-12">
           <td class="border border-black py-2 text-center">{{ idx + 1 }}</td>
-          <td class="border border-black py-2 text-left pl-2">
+          <td class="border border-black py-2 text-left pl-2 text-center">
               <a :href="contest.link" target="_blank">{{ contest.name }}</a>
           </td>
           <td class="grid grid-cols-6 p-0">
-              <div v-for="problem in contest.problems" class="m-1 p-1 rounded flex items-center justify-center text-center h-12 border border-black hover:bg-gray-200 hover:cursor-pointer">
-                  <a :href="problem.link" target="_blank">
+              <div v-for="problem in contest.problems" class="m-1 p-1 rounded flex flex-col items-center justify-center text-center border border-black hover:bg-gray-200 hover:cursor-pointer">
+                  <a target="_blank">
                       {{ problem.index }}. {{ problem.name }}
                   </a>
+                  <div class="flex space-x-6 italic font-mono bg-gray-100">
+                      <div class="h-2 w-2">
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke-width="1.5" stroke="currentColor">
+                              <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+                          </svg>
+                      </div>
+                      <div>[{{ problem.solvedCount }}]</div>
+                  </div>
+                  <!-- <span class="italic font-mono">[{{ problem.points }}]</span> -->
               </div>
           </td>
       </tr>
