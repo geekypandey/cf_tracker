5381642c06cba93f1953eeae5007240d117483d5
Anurag Pandey <anuragcarbon1998@gmail.com>
2024-10-18 18:29:35 +0530

    upgrade model for Contest

--- src/models/Contest.js
+++ src/models/Contest.js
@@ -1,14 +1,28 @@
 import Problem from '@/models/Problem'
 
 export default class Contest {
-  constructor(id, name, div) {
-    this.id = id;
-    this.name = name;
-    this.link = "https://codeforces.com/contest/" + this.id;
-    this.problems = [];
-    this.div = div;
+  constructor(contest) {
+    this.id = contest.id;
+    this.name = contest.name;
+    this.link = `https://codeforces.com/contest/${contest.id}`;
+    this.division = contest.div;
+    this.durationSeconds = contest.durationSeconds;
+    this.startDate = new Date(contest.startTimeSeconds * 1000);
     this.active = false;
     this.passive = false;
+
+    this.display = true;
+    this.isSolved = false;
+    this.problems = [];
+    this._addProblems(contest.problems);
+  }
+
+  _addProblems(problems) {
+      if (problems == undefined) return;
+      // TODO: sort problem based on their indexes
+      for (const problem of problems) {
+        this.addProblem(problem.index, problem.solvedCount, problem.name);
+      }
   }
 
   addProblem(index, solvedCount, name) {
