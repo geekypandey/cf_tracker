818d102cbda90c34a1263a8a768c1f53084fd872
Anurag Pandey <anuragcarbon1998@gmail.com>
2024-10-18 14:44:01 +0530

    update styling for the display contests

--- src/components/DisplayContests.vue
+++ src/components/DisplayContests.vue
@@ -1,5 +1,10 @@
 <script setup>
+import { storeToRefs } from 'pinia'
 import ContestsData from "@/data/contests.json";
+import { useFilterStore } from '@/stores/filters'
+
+const filterStore = useFilterStore()
+const { selectedDivisions } = storeToRefs(filterStore)
 
 const contests = ContestsData.contests;
 </script>
@@ -8,22 +13,17 @@ const contests = ContestsData.contests;
   <div v-if="!contests.length" style="text-align: center">Loading.....</div>
   <table class="w-full">
       <tr v-for="(contest, idx) in contests" class="border border-black h-12">
-          <td class="border border-black py-2 text-center">{{ idx + 1 }}</td>
-          <td class="border border-black py-2 text-left pl-2 text-center">
-              <a :href="contest.link" target="_blank">{{ contest.name }}</a>
+          <td class="border border-black py-2 text-center font-mono">{{ idx + 1 }}</td>
+          <td class="border border-black py-2 pl-2 text-center">
+              <a :href="contest.link" target="_blank" class="font-mono">{{ contest.name }}</a>
           </td>
-          <td class="grid grid-cols-6 p-0">
-              <div v-for="problem in contest.problems" class="m-1 p-1 rounded flex flex-col items-center justify-center text-center border border-black hover:bg-gray-200 hover:cursor-pointer">
+          <td class="grid grid-cols-6 p-0 group">
+              <div v-for="problem in contest.problems" class="m-1 p-1 rounded flex flex-col space-y-1 items-center justify-center text-center border border-black hover:bg-gray-200 hover:cursor-pointer">
                   <a target="_blank">
-                      {{ problem.index }}. {{ problem.name }}
+                      <span class="font-mono">{{ problem.index }}. {{ problem.name }}</span>
                   </a>
-                  <div class="flex space-x-6 italic font-mono bg-gray-100">
-                      <div class="h-2 w-2">
-                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke-width="1.5" stroke="currentColor">
-                              <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
-                          </svg>
-                      </div>
-                      <div>[{{ problem.solvedCount }}]</div>
+                  <div class="flex space-x-6 font-mono">
+                      <span class="font-bold">[{{ problem.solvedCount }}]</span>
                   </div>
                   <!-- <span class="italic font-mono">[{{ problem.points }}]</span> -->
               </div>
@@ -31,7 +31,3 @@ const contests = ContestsData.contests;
       </tr>
   </table>
 </template>
-
-<style scoped>
-</style>
-
