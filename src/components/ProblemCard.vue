552c0523f1501abd345bee47f4c6815143aace85
Anurag Pandey <anuragcarbon1998@gmail.com>
2024-10-24 14:26:30 +0530

    fix issue with checking empty tags

--- src/components/ProblemCard.vue
+++ src/components/ProblemCard.vue
@@ -7,7 +7,7 @@ defineProps({problem: Object})
 <template>
     <div class="flex bg-white flex-col space-y-2 p-6 rounded-lg shadow" v-if="problem">
         <span class="font-semibold hover:underline hover:cursor-pointer"> {{ problem.name }} </span>
-        <div class="flex flex-col space-y-1" v-if="problem.tags.length > 0">
+        <div class="flex flex-col space-y-1" v-if="problem.tags && problem.tags.length > 0">
             <h4>Tags:</h4>
             <ul class="list-disc pl-6">
                 <li v-for="tag in problem.tags" class="text-sm text-gray-500">{{ tag }}</li>
