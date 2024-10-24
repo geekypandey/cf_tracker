6610aeb6ea594fb347c1e1e6b7e858c231872a8e
Anurag Pandey <anuragcarbon1998@gmail.com>
2024-10-24 14:56:23 +0530

    add import for vue virtual scroller

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia';
 
+// imports for vue-virtual-scroller
+import VueVirtualScroller from 'vue-virtual-scroller'
+import 'vue-virtual-scroller/dist/vue-virtual-scroller.css'
+
 import './style.css'
 import router from '@/router'
 import App from './App.vue'
@@ -9,5 +13,6 @@ const app = createApp(App);
 
 app.use(createPinia())
 app.use(router)
+app.use(VueVirtualScroller)
 
 app.mount('#app');
