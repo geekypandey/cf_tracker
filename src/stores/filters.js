295211ef9c6edb6d60bc64a66a06e522c99d0ce6
Anurag Pandey <anuragcarbon1998@gmail.com>
2024-10-25 15:47:35 +0530

    fix bug in filtering
    when the contests participated is chosen and then later all the
    participants are removed, we get no contests to display since the filter
    value seleted not default one

--- src/stores/filters.js
+++ src/stores/filters.js
@@ -1,9 +1,27 @@
-import { ref } from 'vue'
-import { defineStore } from 'pinia'
+import { ref, computed } from 'vue'
+import { defineStore, storeToRefs } from 'pinia'
+
+import { useUserStore } from '@/stores/users'
+
+const DEFAULT_PARTICIPANT = 'any'
 
 export const useFilterStore = defineStore('filter', () => {
+    const userStore = useUserStore()
+    const { users } = storeToRefs(userStore)
+
     const selectedDivisions = ref([])
-    // default participant is any
-    const participantType = ref('any')
+    const participant = ref(DEFAULT_PARTICIPANT)
+
+    const participantType = computed({
+        get() {
+            if (users.value.length > 0) {
+                return participant.value;
+            }
+            return DEFAULT_PARTICIPANT;
+        },
+        set(newValue) {
+            participant.value = newValue
+        }
+    })
     return { selectedDivisions, participantType };
 })
