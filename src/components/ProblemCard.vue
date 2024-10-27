004e10b2ed424e82bf31d1ee21c93214ff7df7b7
Anurag Pandey <anuragcarbon1998@gmail.com>
2024-10-27 10:22:58 +0530

    add search bar and make the card prettier

--- src/components/ProblemCard.vue
+++ src/components/ProblemCard.vue
@@ -5,13 +5,16 @@ defineProps({problem: Object})
 </script>
 
 <template>
-    <div class="flex bg-white flex-col space-y-2 p-6 rounded-lg shadow" v-if="problem">
-        <span class="font-semibold hover:underline hover:cursor-pointer"> {{ problem.name }} </span>
-        <div class="flex flex-col space-y-1" v-if="problem.tags && problem.tags.length > 0">
-            <h4>Tags:</h4>
-            <ul class="list-disc pl-6">
-                <li v-for="tag in problem.tags" class="text-sm text-gray-500">{{ tag }}</li>
-            </ul>
-        </div>
+    <div class="p-4 bg-white rounded-lg shadow" v-if="problem">
+      <h3 class="text-lg font-semibold mb-2">{{ problem.name }}</h3>
+      <div class="flex flex-wrap gap-2">
+        <span
+          v-for="tag in problem.tags"
+          :key="tag"
+          class="px-2 py-1 bg-blue-100 text-blue-700 text-sm rounded-full"
+        >
+          {{ tag }}
+        </span>
+      </div>
     </div>
 </template>
--- src/views/ProblemsetView.vue
+++ src/views/ProblemsetView.vue
@@ -13,9 +13,33 @@ const problemSet = computed(() => {
 </script>
 
 <template>
-    <div class="p-12">
-        <div class="grid grid-cols-4 gap-4">
-            <ProblemCard :problem="problem" v-for="problem in problemSet" />
+    <div class="p-4 bg-white border-b">
+        <div class="relative">
+          <input
+            v-model="searchTerm"
+            type="text"
+            placeholder="Search problems..."
+            class="w-full pl-10 pr-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          <svg
+            class="absolute left-3 top-2.5 h-5 w-5 text-gray-400"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
+        </div>
+    </div>
+    <div class="flex-1 p-4 overflow-y-auto">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+          <ProblemCard v-for="(problem, index) in problemSet" :key="index" :problem="problem" />
         </div>
     </div>
 </template>
