3952a9c4dbe02ee7bdce66180f0384f692fcb3e1
Anurag Pandey <anuragcarbon1998@gmail.com>
2024-10-28 14:40:20 +0530

    move header to separate navigation bar component

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,13 @@ import { ref } from 'vue'
 import { RouterView, RouterLink } from 'vue-router'
 
 import ContestView from '@/views/ContestView.vue'
+import NavigationBar from '@/components/NavigationBar.vue'
 
 const showNewFeatures = ref(false);
 </script>
 
 <template>
-  <header class="flex bg-blue-900 py-2 items-center justify-around">
-    <div>
-      <!-- Change the font of this heading -->
-      <span class="text-3xl font-medium text-white">&lt;CF-TRACKER /&gt;</span>
-    </div>
-  </header>
+  <NavigationBar />
   <main>
     <RouterView />
   </main>
---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,8 @@
+<template>
+  <header class="flex bg-blue-900 py-2 items-center justify-around">
+    <div>
+      <!-- Change the font of this heading -->
+      <span class="text-3xl font-medium text-white">&lt;CF-TRACKER /&gt;</span>
+    </div>
+  </header>
+</template>
