3c078066d053d85fe89d2869e6c81978e0a04235
Anurag Pandey <anuragcarbon1998@gmail.com>
2024-10-29 20:01:04 +0530

    use flex container instead of random number padding

--- src/views/ContestView.vue
+++ src/views/ContestView.vue
@@ -11,8 +11,8 @@ const ranks = ref([])
 </script>
 
 <template>
-  <div>
-    <div class="mx-40 my-8 bg-white dark:bg-gray-800 dark:text-white p-4 rounded-md">
+  <div class="flex items-center justify-center my-8 px-4">
+    <div class="bg-white dark:bg-gray-800 dark:text-white p-4 rounded-md">
       <UserSearch @updateUsers="usernames = $event" @updateRanks="ranks = $event" />
       <UserInfo :usernames="usernames" :ranks="ranks" class="mb-1"/>
       <ContestFilters />
