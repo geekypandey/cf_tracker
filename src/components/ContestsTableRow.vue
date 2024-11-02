8df64510f428441c2972b0caa2438872ed765e79
Anurag Pandey <anuragcarbon1998@gmail.com>
2024-11-02 16:44:40 +0530

    add changes to contests table row

--- src/components/ContestsTableRow.vue
+++ src/components/ContestsTableRow.vue
@@ -6,21 +6,24 @@ defineProps({
 </script>
 
 <template>
-  <div class="flex space-x-2 w-full p-2 items-center pl-4">
-      <div>{{ index }} </div>
-      <div class="py-2 text-center hover:underline hover:cursor-pointer w-96">
-          <a :href="contest.link" target="_blank" class="font-mono text-md">{{ contest.name }}</a>
-      </div>
-      <div class="flex p-0 flex-wrap">
-          <div class="flex flex-col items-center text-center border border-black rounded-md w-20 m-1"
-              :class="[problem.isSolved ? 'bg-solved-green' : '', (problem.isAttempted && !problem.isSolved) ? 'bg-unsolved-red' : '']" v-for="problem in contest.problems">
-              <span class="w-12 hover:underline hover:cursor-pointer flex space-x-1 items-center justify-center">
-                  <a :href="problem.link" target="_blank" class="font-mono text-lg" :class="{ 'dark:text-black': problem.isAttempted }">{{ problem.index }}</a>
-                  <span v-if="problem.isSolvedInContest" class="text-black">&#10003;</span>
-                  <span v-if="contest.isContestant && problem.isAttempted && !problem.isSolved" class="dark:text-black">&#10008;</span>
-              </span>
-              <span class="text-sm text-gray-500 font-mono" :class="[problem.isSolved ? 'dark:text-gray-600' : 'dark:text-gray-400']"> {{ problem.solvedCount }} </span>
-          </div>
-      </div>
-  </div>
+    <div class="flex items-center p-2 gap-3">
+        <span class="w-8 text-center shrink-0">{{ index }}</span>
+        <div class="flex flex-col w-full md:flex-row md:items-center">
+            <a :href="contest.link" class="font-mono break-words md:w-96 xl:w-[32rem] shrink-0" target="_blank">{{ contest.name }}</a>
+
+            <div class="flex flex-wrap gap-1">
+                <div class="flex flex-col items-center w-14 h-14 justify-center border border-gray-900 rounded-md" 
+                    :class="[problem.isSolved ? 'bg-solved-green' : (problem.isAttempted ? 'bg-unsolved-red' : '')]"
+                    v-for="problem in contest.problems">
+                    <div class="flex gap-0.5">
+                        <a :href="problem.link" :class="{ 'dark:text-gray-800' : problem.isAttempted }" target="_blank">{{ problem.index }}</a>
+                        <span v-if="problem.isSolvedInContest" class="dark:text-black">&#10003;</span>
+                        <span v-if="problem.isContestant && !problem.isSolvedInContest" class="dark:text-black">&#10008;</span>
+                    </div>
+                    <span class="text-xs dark:text-gray-500 text-gray-600" v-if="problem.solvedCount > 0">[{{ problem.solvedCount }}]</span>
+                </div>
+            </div>
+
+        </div>
+    </div>
 </template>
