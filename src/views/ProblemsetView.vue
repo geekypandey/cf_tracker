aee2abcd2cb2cabc8622be48f217e60832cc0d8c
Anurag Pandey <anuragcarbon1998@gmail.com>
2024-11-11 16:35:53 +0530

    update problemset view page

--- src/views/ProblemsetView.vue
+++ src/views/ProblemsetView.vue
@@ -5,41 +5,69 @@ import ProblemCard from '@/components/ProblemCard.vue'
 
 const contests = ref(ContestsData.contests)
 
+const problemNameSearch = ref('')
+const selectedTags = ref([])
+
 const problemSet = computed(() => {
-    return contests.value
+    const problems = contests.value
         .map(contest => contest.problems)
         .flat()
+		.filter(problem => problem != undefined)
+		.slice(0, 20)
+
+	return problems
+})
+
+const tags = computed(() => {
+	// TODO: improve this tags sorting
+	const tagList = problemSet.value.map(problem => problem.tags)
+							.flat(1)
+	return [...new Set(tagList)]
 })
 </script>
 
 <template>
+	<div class="flex gap-2 min-h-screen">
+		<div class="flex flex-col gap-2 border-2 bg-white">
+			<div class="relative p-2">
+			  <input
+				v-model="problemNameSearch"
+				type="text"
+				placeholder="Search problems..."
+				class="w-full pl-10 pr-4 py-1 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+			  />
+			  <svg
+				class="absolute left-3 top-3.5 h-5 w-5 text-gray-400"
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="currentColor"
+			  >
+				<path
+				  stroke-linecap="round"
+				  stroke-linejoin="round"
+				  stroke-width="2"
+				  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+				/>
+			  </svg>
+			</div>
+			<div class="p-2 gap-2 flex flex-col">
+				<div class="text">Tags</div>
+				<ul>
+					<li class="text-sm text-zinc-600 flex gap-1" v-for="tag in tags" :key="tag">
+						<input type="checkbox" :id="tag" :value="tag" v-model="selectedTags"/>
+						<label :for="tag">{{ tag }}</label>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="flex-1 p-4 overflow-y-auto">
+			<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+			  <ProblemCard v-for="(problem, index) in problemSet" :key="index" :problem="problem" />
+			</div>
+		</div>
+	</div>
+
     <div class="p-4 bg-white border-b">
-        <div class="relative">
-          <input
-            v-model="searchTerm"
-            type="text"
-            placeholder="Search problems..."
-            class="w-full pl-10 pr-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-          <svg
-            class="absolute left-3 top-2.5 h-5 w-5 text-gray-400"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            />
-          </svg>
-        </div>
-    </div>
-    <div class="flex-1 p-4 overflow-y-auto">
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-          <ProblemCard v-for="(problem, index) in problemSet" :key="index" :problem="problem" />
-        </div>
     </div>
 </template>
