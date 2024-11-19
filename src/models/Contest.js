e023b522daaa0beab83ef16b1824faea29de8254
Anurag Pandey <anuragcarbon1998@gmail.com>
2024-11-19 12:35:10 +0530

    update link in contest model based on phase
    when the contests are not finished, and there is less than or equal to 2
    weeks remaining of the competition the contest registration link should
    be displayed.

--- src/models/Contest.js
+++ src/models/Contest.js
@@ -1,20 +1,37 @@
 import Problem from '@/models/Problem'
 
 export default class Contest {
+  #EMPTY_LINK = '';
+
   constructor(contest) {
     this.id = contest.id;
     this.name = contest.name;
-    this.link = `https://codeforces.com/contest/${contest.id}`;
     this.division = contest.div;
     this.durationSeconds = contest.durationSeconds;
     this.startDate = new Date(contest.startTimeSeconds * 1000);
     this.active = false;
     this.passive = false;
+    this.phase = contest.phase;
 
     this.contestants = new Set();
     this.participants = new Set();
     this.problems = [];
     this._addProblems(contest.problems);
+  }
+
+  #weeksBeforeStart() {
+    const today = new Date();
+    return Math.ceil((this.startDate - today) / (7 * 24 * 60 * 60 * 1000));
+  }
+
+  get link() {
+    if (this.phase === 'BEFORE' && this.#weeksBeforeStart() <= 2) {
+      return `https://codeforces.com/contestRegistration/${this.id}`;
+    } else if (this.phase !== 'BEFORE') {
+      return `https://codeforces.com/contest/${this.id}`;
+    } else {
+      return this.#EMPTY_LINK;
+    }
   }
 
   _addProblems(problems) {
